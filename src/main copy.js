ab23ed64051dd5b95dee318c630577a342ea63d3
filippofoladore <filippo.foladore@gmail.com>
2021-01-18 22:29:22 +0100

    Main.js empty

---
+++ src/main copy.js
@@ -0,0 +1,35 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./routes/index";
+import firebase from "firebase"
+import store from "./store";
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+
+Vue.use(VueMaterial)
+
+Vue.config.productionTip = false;
+
+const configOptions = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
+firebase.initializeApp(configOptions);
+
+firebase.auth().onAuthStateChanged(user => {
+  store.dispatch("fetchUser", user);
+});
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
